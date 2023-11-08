ffd4129e7dded5471b73a877267a8d471533e374
vino74478 <vkumar74478@gmail.com>
2023-11-08 10:19:28 +0530

    DAY 8 & 9 practice problems

--- SQL/DAY - 8.sql
+++ SQL/DAY - 8.sql
@@ -1,3 +1,4 @@
+-- WEATHER OBSERVATION STATION 3
 -- Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
 -- The STATION table is described as follows:
 
